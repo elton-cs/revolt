8579fbda0a5839a0b5cb90cc4a788c7ee2801330
elton-cs <govada.elton@gmail.com>
2024-08-24 15:47:11 -0400

    Add simple map matrix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
-use revolt::plugins::ONE;
+use revolt::{plugins::ONE, utils::map_gen::{generate_map, MapPlugin}};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_plugins(MapPlugin);
     app.add_systems(Update, print_one);
     app.run();
 }
--- src/utils/map_gen.rs
+++ src/utils/map_gen.rs
@@ -1,4 +1,31 @@
-pub fn generate_map_matrix() -> Vec<Vec<i32>> {
-    35w x 35h
-    let mut map_matrix = vec![vec![0; 35]; 35];
+use bevy::prelude::*;
+
+pub struct MapPlugin;
+
+impl Plugin for MapPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, generate_map);
+    }
+}
+
+#[derive(Resource)]
+pub struct MapResource{
+    matrix: [[u32;9];9],  
+}
+
+pub fn generate_map(mut commands: Commands) {
+    let map_matrix = [
+        [1,1,1,1,1,1,1,1,1],
+        [1,1,1,0,0,0,1,1,1],
+        [1,1,0,0,0,0,0,1,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,1],
+        [1,1,0,0,0,0,0,1,1],
+        [1,1,1,0,0,0,1,1,1],
+        [1,1,1,1,1,1,1,1,1],
+    ];
+
+    commands.insert_resource(MapResource {matrix: map_matrix});
+
 }
