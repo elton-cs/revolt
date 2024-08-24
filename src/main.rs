d36f01ac85cc2c8aac3b7b34165968c32111761f
elton-cs <govada.elton@gmail.com>
2024-08-24 17:15:33 -0400

    Render basic map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use bevy::prelude::*;
-use revolt::{plugins::ONE, utils::map_gen::{generate_map, MapPlugin}};
+use revolt::utils::map_gen::MapPlugin;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
+    app.add_systems(PreStartup, default_camera);
     app.add_plugins(MapPlugin);
-    app.add_systems(Update, print_one);
     app.run();
 }
 
-fn print_one() {
-    info!("{ONE}");
+fn default_camera(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scale = 0.2;
+    commands.spawn(camera_bundle);
 }
--- src/utils/map_gen.rs
+++ src/utils/map_gen.rs
@@ -1,31 +1,102 @@
 use bevy::prelude::*;
 
+const TILE_SIZE: f32 = 8.0;
+
 pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, generate_map);
+        app.add_systems(Startup, (generate_map, render_map).chain());
     }
 }
 
 #[derive(Resource)]
-pub struct MapResource{
-    matrix: [[u32;9];9],  
+pub struct MapResource {
+    matrix: [[u32; 9]; 9],
 }
 
 pub fn generate_map(mut commands: Commands) {
     let map_matrix = [
-        [1,1,1,1,1,1,1,1,1],
-        [1,1,1,0,0,0,1,1,1],
-        [1,1,0,0,0,0,0,1,1],
-        [1,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,1],
-        [1,1,0,0,0,0,0,1,1],
-        [1,1,1,0,0,0,1,1,1],
-        [1,1,1,1,1,1,1,1,1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
 
-    commands.insert_resource(MapResource {matrix: map_matrix});
+    commands.insert_resource(MapResource { matrix: map_matrix });
+}
 
+fn render_map(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    map: Res<MapResource>,
+) {
+    // let map_texture = asset_server.load("cave_32c_24r.png");
+    // let map_layout = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 32, 24, None, None);
+
+    let map_texture = asset_server.load("2_tiles.png");
+    let map_layout = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 2, 1, None, None);
+
+    let texture_atlas_layout_handle = texture_atlas_layouts.add(map_layout);
+
+    for (y, row) in map.matrix.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            // let tile_index = match tile {
+            //     0 => 134,
+            //     1 => 353,
+            //     // 0 => 0,
+            //     // 1 => 1,
+            //     _ => 0,
+            // };
+
+            let mut transform = Transform::from_translation(Vec3::new(
+                x as f32 * TILE_SIZE,
+                y as f32 * TILE_SIZE,
+                0.0,
+            ));
+            transform.scale = Vec3::splat(0.5);
+
+            let texture_atlas = TextureAtlas {
+                layout: texture_atlas_layout_handle.clone(),
+                index: 0,
+            };
+
+            let sprite_bundle = SpriteBundle {
+                transform,
+                texture: map_texture.clone(),
+                ..default()
+            };
+
+            commands.spawn((texture_atlas, sprite_bundle));
+
+            match tile {
+                1 => {
+                    let mut transform = Transform::from_translation(Vec3::new(
+                        x as f32 * TILE_SIZE,
+                        y as f32 * TILE_SIZE,
+                        1.0,
+                    ));
+                    transform.scale = Vec3::splat(0.5);
+                    let texture_atlas = TextureAtlas {
+                        layout: texture_atlas_layout_handle.clone(),
+                        index: 1,
+                    };
+                    let sprite_bundle = SpriteBundle {
+                        transform,
+                        texture: map_texture.clone(),
+                        ..default()
+                    };
+
+                    commands.spawn((texture_atlas, sprite_bundle));
+                }
+                _ => {}
+            };
+        }
+    }
 }
