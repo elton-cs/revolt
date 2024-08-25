2fefdf6279e5832a20b1c908b2d7ccbb69dbe3fe
elton-cs <govada.elton@gmail.com>
2024-08-25 16:12:49 -0400

    Clean up unused code and assets

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
     app.add_systems(PreStartup, default_camera);
+    app.add_systems(Update, display_num_entities);
     app.add_plugins(MapPlugin);
     app.run();
 }
@@ -14,4 +15,10 @@ fn default_camera(mut commands: Commands) {
     camera_bundle.transform.translation = Vec3::new(120.0, 240.0, 1.0);
     camera_bundle.projection.scale = 0.5;
     commands.spawn(camera_bundle);
+}
+
+fn display_num_entities(query: Query<Entity>) {
+    let num_entities = query.iter().count();
+
+    println!("Number of entities: {}", num_entities);
 }
--- src/utils/map_gen.rs
+++ src/utils/map_gen.rs
@@ -1,21 +1,12 @@
+use super::img_to_matrix::pixel_convertor;
 use bevy::prelude::*;
-use rand::Rng;
-
-use super::img_to_matrix::{convertor, pixel_convertor};
 
 const TILE_SIZE: f32 = 8.0;
-const WIDTH: usize = 20;
-const HEIGHT: usize = 20;
-const WALL_PROBABILITY: f32 = 0.45;
-const SMOOTHING_ITERATIONS: usize = 5;
 
 pub struct MapPlugin;
-
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_systems(Startup, (generate_manual_map, render_map).chain());
-        // app.add_systems(Startup, (generate_random_map, render_map).chain());
-        app.add_systems(Startup, (gen_map_1, render_map).chain());
+        app.add_systems(Startup, (gen_map_from_img, render_map).chain());
     }
 }
 
@@ -24,33 +15,9 @@ pub struct MapResource {
     matrix: Vec<Vec<u32>>,
 }
 
-pub fn generate_manual_map(mut commands: Commands) {
-    let map_matrix = vec![
-        vec![1, 1, 1, 1, 1, 1, 1, 1, 1],
-        vec![1, 1, 1, 0, 0, 0, 1, 1, 1],
-        vec![1, 1, 0, 0, 0, 0, 0, 1, 1],
-        vec![1, 0, 0, 0, 0, 0, 0, 0, 1],
-        vec![1, 0, 0, 1, 1, 1, 0, 0, 1],
-        vec![1, 0, 0, 0, 0, 0, 0, 0, 1],
-        vec![1, 1, 0, 0, 0, 0, 0, 1, 1],
-        vec![1, 1, 1, 0, 0, 0, 1, 1, 1],
-        vec![1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
-
+pub fn gen_map_from_img(mut commands: Commands) {
+    let map_matrix = pixel_convertor("assets/dungeon_pixelated.png");
     commands.insert_resource(MapResource { matrix: map_matrix });
-}
-
-pub fn gen_map_1(mut commands: Commands) {
-    // let map_matrix = convertor("src/dungeon_35x45.png");
-    let map_matrix = pixel_convertor("src/dungeon_pixelated.png");
-
-    commands.insert_resource(MapResource { matrix: map_matrix });
-}
-
-pub fn generate_random_map(mut commands: Commands) {
-    let map = generate_dungeon_map();
-
-    commands.insert_resource(MapResource { matrix: map });
 }
 
 fn render_map(
@@ -59,9 +26,6 @@ fn render_map(
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
     map: Res<MapResource>,
 ) {
-    // let map_texture = asset_server.load("cave_32c_24r.png");
-    // let map_layout = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 32, 24, None, None);
-
     let map_texture = asset_server.load("2_tiles.png");
     let map_layout = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 2, 1, None, None);
 
@@ -69,14 +33,6 @@ fn render_map(
 
     for (y, row) in map.matrix.iter().enumerate() {
         for (x, tile) in row.iter().enumerate() {
-            // let tile_index = match tile {
-            //     0 => 134,
-            //     1 => 353,
-            //     // 0 => 0,
-            //     // 1 => 1,
-            //     _ => 0,
-            // };
-
             let mut transform = Transform::from_translation(Vec3::new(
                 x as f32 * TILE_SIZE,
                 y as f32 * TILE_SIZE,
@@ -121,68 +77,4 @@ fn render_map(
             };
         }
     }
-}
-
-fn generate_dungeon_map() -> Vec<Vec<u32>> {
-    let mut rng = rand::thread_rng();
-    let mut map = vec![vec![0; WIDTH]; HEIGHT];
-
-    // Initialize the map with random walls
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            if rng.gen::<f32>() < WALL_PROBABILITY {
-                map[y][x] = 1;
-            }
-        }
-    }
-
-    // Apply cellular automaton rules
-    for _ in 0..SMOOTHING_ITERATIONS {
-        map = smooth_map(&map);
-    }
-
-    map
-}
-
-fn smooth_map(map: &Vec<Vec<u32>>) -> Vec<Vec<u32>> {
-    let mut new_map = map.clone();
-
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            let wall_count = count_walls_around(map, x, y);
-
-            if wall_count > 4 {
-                new_map[y][x] = 1;
-            } else if wall_count < 4 {
-                new_map[y][x] = 0;
-            }
-        }
-    }
-
-    new_map
-}
-
-fn count_walls_around(map: &Vec<Vec<u32>>, x: usize, y: usize) -> usize {
-    let mut count = 0;
-
-    for dy in -1..=1 {
-        for dx in -1..=1 {
-            if dx == 0 && dy == 0 {
-                continue;
-            }
-
-            let nx = x as isize + dx;
-            let ny = y as isize + dy;
-
-            if nx >= 0 && nx < WIDTH as isize && ny >= 0 && ny < HEIGHT as isize {
-                if map[ny as usize][nx as usize] == 1 {
-                    count += 1;
-                }
-            } else {
-                count += 1; // Treat out-of-bounds as walls
-            }
-        }
-    }
-
-    count
 }
