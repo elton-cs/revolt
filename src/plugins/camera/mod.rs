96d3a01a72cf463e2590ea4ef0497c75ab1ba87d
elton-cs <govada.elton@gmail.com>
2024-08-27 15:21:33 -0400

    Create camera plugin

---
+++ src/plugins/camera/mod.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+pub struct CenteredCameraPlugin;
+impl Plugin for CenteredCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, default_camera);
+    }
+}
+
+fn default_camera(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.transform.translation = Vec3::new(140.0, 80.0, 0.0);
+    camera_bundle.projection.scale = 0.3;
+    commands.spawn(camera_bundle);
+}
