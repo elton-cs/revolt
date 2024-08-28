b5e86ff851477054cc0ba35f2d3affd40ed5e243
elton-cs <govada.elton@gmail.com>
2024-08-28 14:25:24 -0400

    Implement more type extractor functions

--- src/plugins/dojo_to_bevy/type_extractors.rs
+++ src/plugins/dojo_to_bevy/type_extractors.rs
@@ -1,7 +1,16 @@
 use dojo_types::schema::Member;
+use starknet_crypto::Felt;
+
+pub fn member_to_bool(member: &Member) -> bool {
+    member.ty.as_primitive().unwrap().as_bool().unwrap().clone()
+}
 
 pub fn member_to_u8(member: &Member) -> u8 {
     member.ty.as_primitive().unwrap().as_u8().unwrap().clone()
+}
+
+pub fn member_to_u16(member: &Member) -> u16 {
+    member.ty.as_primitive().unwrap().as_u16().unwrap().clone()
 }
 
 pub fn member_to_u32(member: &Member) -> u32 {
@@ -10,4 +19,14 @@ pub fn member_to_u32(member: &Member) -> u32 {
 
 pub fn member_to_enum_to_u8(member: &Member) -> u8 {
     member.ty.as_enum().unwrap().option.unwrap().clone()
+}
+
+pub fn member_to_contract_address_to_felt(member: &Member) -> Felt {
+    member
+        .ty
+        .as_primitive()
+        .unwrap()
+        .as_contract_address()
+        .unwrap()
+        .clone()
 }
