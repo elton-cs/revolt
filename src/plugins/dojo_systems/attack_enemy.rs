31e8dafcd9e3cf7207bb92387b3918beb6d5e4da
elton-cs <govada.elton@gmail.com>
2024-08-29 18:37:30 -0400

    Implement working attack function

--- src/plugins/dojo_systems/attack_enemy.rs
+++ src/plugins/dojo_systems/attack_enemy.rs
@@ -1,4 +1,6 @@
 use crate::{
+    plugins::dojo_systems::account::build_account,
+    states::GameStates,
     tokio::TokioRuntime,
     utils::constants::{GAME_SYSTEM_CONTRACT_ADDRESS, GAME_SYSTEM_SELECTORS},
 };
@@ -15,14 +17,23 @@ use starknet::{
 };
 use starknet_crypto::Felt;
 
+use super::account::PlayerAccount;
+
 pub struct AttackEnemy;
 impl Plugin for AttackEnemy {
     fn build(&self, app: &mut App) {
-        // app.add_systems(Update, send_attack_transaction);
+        app.add_systems(
+            Update,
+            send_attack_transaction.run_if(in_state(GameStates::InGame)),
+        );
     }
 }
 
-fn send_attack_transaction(tokio: Res<TokioRuntime>, mut evr_kbd: EventReader<KeyboardInput>) {
+fn send_attack_transaction(
+    tokio: Res<TokioRuntime>,
+    mut evr_kbd: EventReader<KeyboardInput>,
+    player_account: Res<PlayerAccount>,
+) {
     let mut should_execute = false;
 
     for ev in evr_kbd.read() {
@@ -39,24 +50,49 @@ fn send_attack_transaction(tokio: Res<TokioRuntime>, mut evr_kbd: EventReader<Ke
     }
 
     if should_execute {
+        let pk = player_account.pk.clone();
+        let address = player_account.address.clone();
+
         let actions_contract_address = Felt::from_hex(GAME_SYSTEM_CONTRACT_ADDRESS).unwrap();
         let selector = get_selector_from_name(GAME_SYSTEM_SELECTORS[3]).unwrap();
-        let game_id = Felt::from_dec_str("1").unwrap();
 
+        let game_id = Felt::from_dec_str("1").unwrap();
         let calldata = vec![game_id];
 
-        // tokio.runtime.block_on(async move {
-        //     let result = account_res
-        //         .0
-        //         .execute_v1(vec![Call {
-        //             to: actions_contract_address,
-        //             selector,
-        //             calldata,
-        //         }])
-        //         .send()
-        //         .await;
+        tokio.runtime.spawn(async move {
+            let account = build_account(pk.as_str(), address.as_str());
+            let result = account
+                .execute_v1(vec![Call {
+                    to: actions_contract_address,
+                    selector,
+                    calldata,
+                }])
+                .send()
+                .await;
 
-        //     info!("SENT A ATTACK TRANSACTION: {:?}", result);
-        // });
+            info!("SENT AN ATTACK TRANSACTION: {:?}", result);
+        });
     }
+
+    // if should_execute {
+    //     let actions_contract_address = Felt::from_hex(GAME_SYSTEM_CONTRACT_ADDRESS).unwrap();
+    //     let selector = get_selector_from_name(GAME_SYSTEM_SELECTORS[3]).unwrap();
+    //     let game_id = Felt::from_dec_str("1").unwrap();
+
+    //     let calldata = vec![game_id];
+
+    //     tokio.runtime.block_on(async move {
+    //         let result = account_res
+    //             .0
+    //             .execute_v1(vec![Call {
+    //                 to: actions_contract_address,
+    //                 selector,
+    //                 calldata,
+    //             }])
+    //             .send()
+    //             .await;
+
+    //         info!("SENT A ATTACK TRANSACTION: {:?}", result);
+    //     });
+    // }
 }
--- src/plugins/dojo_systems/mod.rs
+++ src/plugins/dojo_systems/mod.rs
@@ -1,4 +1,5 @@
 use account::StarknetRsPlugin;
+use attack_enemy::AttackEnemy;
 use bevy::prelude::*;
 use create_game::CreateGame;
 use join_game::JoinGame;
@@ -14,7 +15,8 @@ impl Plugin for SendTransactionsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(StarknetRsPlugin);
         app.add_plugins(CreateGame);
-        app.add_plugins(MovePlayer);
         app.add_plugins(JoinGame);
+        app.add_plugins(MovePlayer);
+        app.add_plugins(AttackEnemy);
     }
 }
